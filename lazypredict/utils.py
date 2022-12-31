47bc8d2a84fc27379ff656dcc8c258d669dc4068
Jose <jmnunezd123@gmail.com>
2022-12-31 14:35:57 -0300

    removing regression for now as it is already to diferent from classifiers

--- lazypredict/utils.py
+++ lazypredict/utils.py
@@ -1,4 +1,4 @@
-def get_card_split(df, cols, n=5):
+def get_card_split(df, cols, n=6):
     """
     Splits categorical columns into 2 lists based on cardinality (i.e # of unique values)
     Parameters
@@ -16,11 +16,7 @@ def get_card_split(df, cols, n=5):
     card_high : list-like
         Columns with cardinality >= n
     """
-    cond = df[cols].nunique() > n
-    card_high = cols[cond]
-    card_low = cols[~cond]
+    condition = df[cols].nunique() > n
+    card_high = cols[condition]
+    card_low = cols[~condition]
     return card_low, card_high
-
-
-def adjusted_rsquared(r2, n, p):
-    return 1 - (1 - r2) * ((n - 1) / (n - p - 1))
